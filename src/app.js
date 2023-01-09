162adf8a29cf4bc584fa564a9d02db83df3c410a
Jonas0Dias <jonaaasdiaaas@gmail.com>
2023-01-09 16:42:53 -0300

    finalizaado

--- src/app.js
+++ src/app.js
@@ -3,9 +3,38 @@ import cors from 'cors';
 
 const app = express();
 app.use(cors());
+app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.send('testando abertura do servidor pelo navegador')
+
+const tweets = [] //aqui eu to criando uma lista vazia onde vao ter todos os tweets
+const users=[] // msm coisa aqui, com todos os usuarios
+// o user vai ter um nome E avatar... lembrar de dar push no nome e no avatar, nao só no nome
+let avatar={avatar:''}
+app.post('/tweets', (req, res) => {
+    const tweet={username:'', avatar:'', tweet:''}
+    tweet.username = req.body.username // aqui eu crio uma variavel que recebe o conteudo do corpo de quando alguem faz um requisição post
+    tweet.tweet=req.body.tweet
+    
+    const isOn=users.find(u=> u===req.body.username)
+    res.send(tweet);
+    if (!isOn){
+        res.send('UNAUTHORIZED')
+    }
+
+    tweets.push({...tweet, avatar: avatar}); // aqui eu adiciono um tweet na lista de tweets==
+});
+
+app.post('/sign-up', (req, res) => {
+    const user = req.body.username
+    const avatar = req.body.avatar
+    users.push({user, avatar});
+    res.send("ok");
+
+});
+
+app.get("/tweets", (req, res) =>{
+    const Ultimostweets = tweets.reverse()
+    res.send(Ultimostweets.slice(0,11))
 })
 
-app.listen(4000, () => {console.log('teste')});
+app.listen(5000, () => { console.log('teste') });
