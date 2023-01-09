c561ee36d2de9a8a8020d4606629c1b52e8af2a5
Jonas0Dias <jonaaasdiaaas@gmail.com>
2023-01-09 17:39:08 -0300

    ajustes

--- src/app.js
+++ src/app.js
@@ -9,25 +9,19 @@ app.use(express.json())
 const tweets = [] //aqui eu to criando uma lista vazia onde vao ter todos os tweets
 const users=[] // msm coisa aqui, com todos os usuarios
 // o user vai ter um nome E avatar... lembrar de dar push no nome e no avatar, nao só no nome
-let avatar=''
-
-
+let usuarioavatar={avatar:""}
 app.post('/sign-up', (req, res) => {
-    const user = req.body.username
-    avatar = req.body.avatar
-    users.push({username:user, avatar:avatar});
-    res.send(console.log(avatar));
+    const user = req.body
+    users.push(user);
+    usuarioavatar.avatar=user.avatar
+    res.status(200).send("OK")
 
 });
 
-app.get('/sign-up', (req, res) => {
-    res.send(users)
-}) 
-
 app.post('/tweets', (req, res) => {
-    const tweet={username:'', tweet:''}
-    tweet.username = req.body.username // aqui eu crio uma variavel que recebe o conteudo do corpo de quando alguem faz um requisição post
-    tweet.tweet=req.body.tweet
+    const tweet=req.body
+     // aqui eu crio uma variavel que recebe o conteudo do corpo de quando alguem faz um requisição post
+    
     
     const isOn=users.find(u=> u===req.body.username)
     res.send(tweet);
@@ -35,7 +29,18 @@ app.post('/tweets', (req, res) => {
         res.send('UNAUTHORIZED')
     }
 
-    tweets.push({...tweet, avatar: avatar}); // aqui eu adiciono um tweet na lista de tweets==
+    tweets.push({...usuarioavatar, ...tweet}); // aqui eu adiciono um tweet na lista de tweets==
+    res.status(200).send("OK")
 });
+
+app.get('/tweets', (req,res) => {
+    if (tweets.length > 10){
+        const ultimostweets = tweets.slice(-10)
+        return res.send(ultimostweets)
+     }
+
+
+    res.send(tweets)
+})
 
 app.listen(5000, () => { console.log('teste') });
